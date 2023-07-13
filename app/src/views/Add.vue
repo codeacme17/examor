6659b43d88001502745816e0a4a54ecd464790e1
leyoonafr <vir_kings@qq.com>
2023-07-13 11:07:20 +0800

    chore: update `profile` view

--- app/src/views/Add.vue
+++ app/src/views/Add.vue
@@ -1,7 +1,50 @@
 <template>
-  <section>
-    <v-card class="pa-5 border" elevation="0"> Add </v-card>
-  </section>
+  <v-container class="main_width">
+    <h2>Add Note</h2>
+    <h5>Configuration items can be configured here what you need</h5>
+
+    <section class="py-6">
+      <v-select
+        label="Select note type"
+        :items="['Local Files', 'Notion']"
+        variant="outlined"
+        density="compact"
+        class="mt-3"
+      />
+
+      <v-file-input
+        v-model="files"
+        counter
+        label="Select your file"
+        class="mt-3"
+        multiple
+        prepend-inner-icon="mdi-file-document-plus"
+        variant="outlined"
+        :show-size="1000"
+      >
+        <template v-slot:selection="{ fileNames }">
+          <template v-for="(fileName, index) in fileNames" :key="fileName">
+            <v-chip
+              v-if="index < 2"
+              color="primary"
+              label
+              size="small"
+              class="me-2"
+            >
+              {{ fileName }}
+            </v-chip>
+
+            <span
+              v-else-if="index === 2"
+              class="text-overline text-grey-darken-3 mx-2"
+            >
+              +{{ files.length - 2 }} File(s)
+            </span>
+          </template>
+        </template>
+      </v-file-input>
+    </section>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -10,4 +53,19 @@ export default {
 }
 </script>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const files = ref<any[]>([])
+</script>
+
+<style scoped lang="scss">
+:deep(.v-field__field) {
+  height: 38px;
+  font-size: 14px;
+
+  .v-field__input {
+    padding-top: 5px !important;
+  }
+}
+</style>
--- app/src/views/Profile.vue
+++ app/src/views/Profile.vue
@@ -1,72 +1,100 @@
 <template>
-  <v-responsive style="max-width: 800px; margin: 0 auto">
+  <v-container class="main_width">
     <h2>Profile</h2>
     <h5>Configuration items can be configured here what you need</h5>
 
     <form class="py-6">
-      <v-text-field
-        class="mt-3"
-        label="OPENAI_KEY"
-        variant="outlined"
-        density="compact"
-        v-model="state.openaiKey.value"
-        :append-inner-icon="state.openaiKey.show ? 'mdi-eye' : 'mdi-eye-off'"
-        :type="state.openaiKey.show ? 'text' : 'password'"
-        @input="v$.name.$touch"
-        @blur="v$.name.$touch"
-        @click:append-inner="state.openaiKey.show = !state.openaiKey.show"
-      />
+      <!-- OpenAI  -->
+      <div class="d-flex">
+        <OpenaiIcon width="30" class="mb-12 mr-4" />
 
-      <v-tooltip
-        text="openai billing"
-        location="start"
-        :open-delay="3"
-        :open-on-hover="true"
-      >
-        <template v-slot:activator="{ props }">
-          <v-progress-linear
-            :height="9"
-            v-bind="props"
-            color="primary"
-            model-value="20"
-            class="mb-8"
+        <div style="flex: 1">
+          <v-text-field
+            class="mt-3"
+            label="OPENAI_KEY"
+            variant="outlined"
+            density="compact"
+            v-model="state.openaiKey.value"
+            :append-inner-icon="
+              state.openaiKey.show ? 'mdi-eye' : 'mdi-eye-off'
+            "
+            :type="state.openaiKey.show ? 'text' : 'password'"
+            @click:append-inner="state.openaiKey.show = !state.openaiKey.show"
           />
-        </template>
-      </v-tooltip>
 
-      <v-text-field
-        class="mt-3"
-        label="PINECONE_KEY"
-        variant="outlined"
-        density="compact"
-        v-model="state.pineconeKey.value"
-        :append-inner-icon="state.pineconeKey.show ? 'mdi-eye' : 'mdi-eye-off'"
-        :type="state.pineconeKey.show ? 'text' : 'password'"
-        @input="v$.name.$touch"
-        @blur="v$.name.$touch"
-        @click:append-inner="state.pineconeKey.show = !state.pineconeKey.show"
-      />
+          <v-tooltip
+            text="openai billing"
+            location="start"
+            :open-delay="3"
+            :open-on-hover="true"
+          >
+            <template v-slot:activator="{ props }">
+              <v-progress-linear
+                :height="6"
+                v-bind="props"
+                color="primary"
+                model-value="20"
+                class="mb-8"
+              />
+            </template>
+          </v-tooltip>
+        </div>
+      </div>
 
-      <div class="mt-5">
+      <!-- Pinecone -->
+      <div class="d-flex">
+        <PineconeIcon width="30" class="mb-3 mr-4" />
+
+        <v-text-field
+          class="mt-3"
+          label="PINECONE_KEY"
+          variant="outlined"
+          density="compact"
+          v-model="state.pineconeKey.value"
+          :append-inner-icon="
+            state.pineconeKey.show ? 'mdi-eye' : 'mdi-eye-off'
+          "
+          :type="state.pineconeKey.show ? 'text' : 'password'"
+          @click:append-inner="state.pineconeKey.show = !state.pineconeKey.show"
+        />
+      </div>
+
+      <!-- Notion -->
+      <div class="d-flex mt-3">
+        <NotionIcon width="30" class="mb-5 mr-4" />
+
+        <v-text-field
+          label="NOTION_KEY"
+          variant="outlined"
+          density="compact"
+          v-model="state.notionKey.value"
+          :append-inner-icon="state.notionKey.show ? 'mdi-eye' : 'mdi-eye-off'"
+          :type="state.notionKey.show ? 'text' : 'password'"
+          @click:append-inner="state.notionKey.show = !state.notionKey.show"
+        />
+      </div>
+
+      <div class="mt-5 d-flex justify-end">
         <v-btn
-          class="me-4"
-          @click="v$.$validate"
           color="primary"
-          width="120"
           elevation="0"
+          :block="true"
+          @click="v$.$validate"
         >
           submit
         </v-btn>
-        <v-btn @click="clear" border="" elevation="0"> clear </v-btn>
       </div>
     </form>
-  </v-responsive>
+  </v-container>
 </template>
 
 <script setup lang="ts">
 import { reactive } from 'vue'
 import { useVuelidate } from '@vuelidate/core'
 import { email, required } from '@vuelidate/validators'
+import OpenaiIcon from '@/components/icons/Openai.vue'
+import PineconeIcon from '@/components/icons/Pinecone.vue'
+import NotionIcon from '@/components/icons/Notion.vue'
 
 const initialState = {
   openaiKey: {
@@ -74,6 +102,10 @@ const initialState = {
     show: false,
   },
   pineconeKey: {
+    value: '',
+    show: false,
+  },
+  notionKey: {
     value: '',
     show: false,
   },
