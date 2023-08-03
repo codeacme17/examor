a7b0c5af9a31574ea004a20ba01308bf7179d15c
codeacme17 <vir_kings@qq.com>
2023-08-03 18:25:10 +0800

    style: update Nprogress style

--- app/src/plugins/nprogress.ts
+++ app/src/plugins/nprogress.ts
@@ -1,11 +1,6 @@
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 
-NProgress.configure({
-  showSpinner: false,
-  template:
-    '<div class="bar" id="nprogess-bar" role="bar"><div class="peg"></div></div><div class="spinner" role="spinner"><div class="spinner-icon"></div></div>',
-  speed: 3000,
-})
+NProgress.configure({})
 
 export default NProgress
