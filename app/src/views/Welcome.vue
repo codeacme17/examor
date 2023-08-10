2a836f10508c255cdbba8a3756ab8907b44b1a53
leyoonafr <vir_kings@qq.com>
2023-08-10 10:36:59 +0800

    feat-app: update `Welcome` page

--- app/src/views/Welcome.vue
+++ app/src/views/Welcome.vue
@@ -1,23 +1,32 @@
 <template>
-  <spline-viewer
-    loading-anim
-    url="https://prod.spline.design/DPERWwhvUp3zeOP8/scene.splinecode"
-    style="
-      position: absolute;
-      width: 100%;
-      height: calc(100vh + 30px);
-      left: 0;
-      top: -30px;
-    "
-  ></spline-viewer>
+  <section
+    class="d-flex justify-center"
+    style="padding-top: 100px; padding-right: 200px"
+  >
+    <div style="width: 200px; height: 200px" class="mr-10">
+      <v-img
+        v-show="isDark"
+        src="/src/assets/images/welcome-dark.svg"
+        :lazy-src="'/src/assets/images/welcome-dark.svg'"
+      />
+      <v-img
+        v-show="!isDark"
+        src="/src/assets/images/welcome-light.svg"
+        :lazy-src="'/src/assets/images/welcome-light.svg'"
+      />
+    </div>
+
+    <div>
+      <h2 class="mb-4">WELCOME</h2>
+      <h4 class="mb-2">Hi I'm leyoo,</h4>
+      <h4 class="mb-2">hope you enjoy this app</h4>
+      <h4>If you like this app, I hope you can give me a ⭐</h4>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
+import { useDark } from '@vueuse/core'
 localStorage.setItem('isWelcome', JSON.stringify(true))
+const isDark = useDark()
 </script>
-
-<style scoped lang="scss">
-:deep(#logo) {
-  display: none !important;
-}
-</style>
