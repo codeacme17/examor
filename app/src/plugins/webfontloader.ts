9455d3ebbaa21569cd7d2d316430aecce31e0ccf
codeacme17 <vir_kings@qq.com>
2023-08-26 12:22:43 +0800

    feat: add `Poppins` & `JetBrains Mono` font families

--- app/src/plugins/webfontloader.ts
+++ app/src/plugins/webfontloader.ts
@@ -1,11 +1,14 @@
 export async function loadFonts() {
   const webFontLoader = await import(
-    /* webpackChunkName: "webfontloader" */ "webfontloader"
-  );
+    /* webpackChunkName: "webfontloader" */ 'webfontloader'
+  )
 
   webFontLoader.load({
     google: {
-      families: ["Roboto:100,300,400,500,700,900&display=swap"],
+      families: [
+        'Poppins:100,300,400,500,700,900&display=swap',
+        'JetBrains Mono',
+      ],
     },
-  });
+  })
 }
