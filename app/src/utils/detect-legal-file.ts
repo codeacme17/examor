891ed727b711c6b59e3c6899f7fa08f0df773c30
codeacme17 <vir_kings@qq.com>
2023-09-03 23:13:39 +0800

    fix: fix issue #31

--- app/src/utils/detect-legal-file.ts
+++ app/src/utils/detect-legal-file.ts
@@ -1,5 +1,12 @@
 import { MessagePlugin, type UploadFile } from 'tdesign-vue-next'
 
+const MIME = {
+  MARKDOWN: 'text/markdown',
+  X_MARKDOWN: 'text/x-markdown',
+  TEXT_PLAIN: 'text/plain',
+  OCTET_STREAM: 'application/octet-stream',
+}
+
 /**
  * Checks if a given file has a valid type for uploading.
  *
@@ -7,10 +14,28 @@ import { MessagePlugin, type UploadFile } from 'tdesign-vue-next'
  * @returns Returns true if the file type is valid, otherwise returns false.
  */
 export const detectLegalFile = (file: UploadFile): boolean => {
-  if (file.type !== 'text/markdown' && file.type !== 'text/x-markdown') {
+  if (!isValidMarkdownFile(file)) {
     MessagePlugin.warning(
-      `The type of file '${file.name}' is wrong, Only '.md' type files are allowed to be uploaded`
+      `The type of file '${file.name}' is wrong, Only "Markdown" type files are allowed to be uploaded`
     )
     return false
   } else return true
+}
+
+/**
+ * Checks if a given file is a valid Markdown file based on its name and MIME type.
+ * @param file - The file to check.
+ * @returns Returns true if the file is a valid Markdown file, otherwise false.
+ */
+const isValidMarkdownFile = (file: UploadFile): boolean => {
+  const { name, type } = file
+  if (!name!.endsWith('.md')) return false
+  if (
+    type === MIME.MARKDOWN ||
+    type === MIME.X_MARKDOWN ||
+    type === MIME.TEXT_PLAIN ||
+    type === MIME.OCTET_STREAM
+  ) {
+    return true
+  } else return false
 }
