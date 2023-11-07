5447703098afaa1dc4a694a3d5b858593cc0a557
codeacme17 <vir_kings@qq.com>
2023-11-07 19:01:56 +0800

    feat: add `gpt-4-1106-preview` model

--- app/src/views/Profile.vue
+++ app/src/views/Profile.vue
@@ -103,7 +103,7 @@
             density="compact"
             :base-color="formData.openaiModel.error ? orangeBgColor : ''"
             :color="formData.openaiModel.error ? orangeBgColor : ''"
-            :items="['gpt-3.5-turbo', 'gpt-4']"
+            :items="['gpt-3.5-turbo', 'gpt-4', 'gpt-4-1106-preview']"
           />
 
           <!-- OpenAI API KEY -->
