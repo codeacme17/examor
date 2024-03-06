6fac8db470c06531249c886fc99ddaade0fb72bf
leyoonafr <vir_kings@qq.com>
2024-03-06 10:53:39 +0800

    chore: init project

---
+++ next/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button variant="outline">Button</Button>
+    </main>
+  )
+}
