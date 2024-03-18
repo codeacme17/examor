820d6a37a20dc29913f8c2b49c5783005dadccc0
leyoonafr <vir_kings@qq.com>
2024-03-18 17:09:29 +0800

    ui: update menu-bar

--- next/components/layout/navbar/menu-bar.tsx
+++ next/components/layout/navbar/menu-bar.tsx
@@ -1,7 +1,9 @@
 'use client'
 
-import * as React from 'react'
+import { memo, forwardRef } from 'react'
 
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { cn } from '@/lib/utils'
 import {
   NavigationMenu,
@@ -14,7 +16,7 @@ import {
 import { MenuItem, useMenu } from '@/hooks/useMenu'
 import { Plus } from 'lucide-react'
 
-export function Menubar() {
+export const Menubar = memo(() => {
   const { staticMenus, noteMenus } = useMenu()
 
   return (
@@ -42,7 +44,7 @@ export function Menubar() {
             <ul className="grid min-w-[180px] gap-1 p-2">
               <ListItem
                 title={'Add new note'}
-                href={'/note/new'}
+                href={'/add-new'}
                 outLine
                 icon={Plus}
               />
@@ -53,6 +55,7 @@ export function Menubar() {
                   title={item.title}
                   href={item.path}
                   icon={item.icon}
+                  loading={item.isLoading}
                 />
               ))}
             </ul>
@@ -61,35 +64,37 @@ export function Menubar() {
       </NavigationMenuList>
     </NavigationMenu>
   )
-}
-
-const ListItem = React.forwardRef<
-  React.ElementRef<'a'>,
-  React.ComponentPropsWithoutRef<'a'> & {
-    icon: MenuItem['icon']
-    outLine?: boolean
-  }
->(({ className, title, outLine, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            'flex items-center select-none rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
-            outLine && 'border border-muted',
-            className
-          )}
-          {...props}>
-          <props.icon className="mr-2" size={18} />
-
-          <span className="text-sm font-medium leading-none">
-            {title}
-          </span>
-        </a>
-      </NavigationMenuLink>
-    </li>
-  )
 })
+
+const ListItem = forwardRef<MenuItem & { outLine?: boolean }, any>(
+  ({ className, title, outLine, ...props }, ref) => {
+    const pathname = usePathname()
+
+    return (
+      <li>
+        <NavigationMenuLink asChild>
+          <Link
+            ref={ref}
+            url={props.href}
+            className={cn(
+              'flex items-center select-none rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
+              outLine && 'border border-muted',
+              pathname === props.href && 'bg-muted',
+              className
+            )}
+            {...props}>
+            <props.icon className="mr-2" size={18} />
+
+            <span className="text-sm font-medium leading-none">
+              {title}
+            </span>
+          </Link>
+        </NavigationMenuLink>
+      </li>
+    )
+  }
+)
+
+Menubar.displayName = 'Menubar'
 
 ListItem.displayName = 'ListItem'
