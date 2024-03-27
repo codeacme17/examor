0e8d6adebcc54e510ba7e7420c92a91a5334540a
codeacme17 <vir_kings@qq.com>
2024-03-27 15:56:55 +0800

    refactor: update configuration files and database processing logic

--- next/lib/db-handler/index.ts
+++ next/lib/db-handler/index.ts
@@ -1,1 +1,11 @@
+import { PrismaClient } from '@prisma/client'
+
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
+export const prismadb = globalThis.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV === 'development') globalThis.prisma = prismadb
+
 export { profileHandler } from './profile'
--- next/lib/prismadb.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-
-declare global {
-  var prisma: PrismaClient | undefined
-}
-
-export const prismadb = globalThis.prisma || new PrismaClient()
-
-if (process.env.NODE_ENV === 'development') globalThis.prisma = prismadb
