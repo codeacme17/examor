2402fb855df7cc99a209d8d517494d8098d79bc6
codeacme17 <vir_kings@qq.com>
2024-04-02 17:52:16 +0800

    ui: update uploading-popup component

--- next/components/share/uploading-popup.tsx
+++ next/components/share/uploading-popup.tsx
@@ -1,62 +1,50 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
+import { Card } from '@/components/ui/card'
 import { useFileStore } from '@/store'
 import { useState } from 'react'
 import { motion } from 'framer-motion'
-import { UploadCloud } from 'lucide-react'
+import { UploadCloud, X, Paperclip } from 'lucide-react'
 import { Button } from '../ui/button'
 
 export const UploadingPopup = () => {
   const fileStore = useFileStore()
   const [isOpen, setIsOpen] = useState(false)
-  // const uploadingFiles = fileStore.uploadingFiles
-
-  const uploadingFiles = [
-    {
-      id: 1,
-      fileName: 'file1',
-    },
-    {
-      id: 2,
-      fileName: 'file2',
-    },
-    {
-      id: 3,
-      fileName: 'file3',
-    },
-  ]
+  const uploadingFiles = fileStore.uploadingFiles
 
   if (uploadingFiles.length === 0) return null
 
   if (!isOpen)
     return (
-      <Button
-        className="fixed rounded-full bottom-10 right-8 p-3"
-        onClick={() => setIsOpen(true)}
-        size={'icon'}>
-        <UploadCloud />
-      </Button>
+      <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{}}>
+        <Button
+          className="fixed rounded-full top-16 right-8 p-3  animate-pulse"
+          onClick={() => setIsOpen(true)}
+          size={'icon'}>
+          <UploadCloud />
+        </Button>
+      </motion.div>
     )
 
   return (
-    <motion.div initial={{ y: 100 }} animate={{ y: 0 }} transition={{}}>
-      <Card className="fixed bottom-10 right-8 p-3">
-        <div className="flex items-center">
-          <UploadCloud className="mr-2" /> Uploading
-          <Button className="ml-auto" onClick={() => setIsOpen(false)}>
-            Close
-          </Button>
+    <motion.div
+      initial={{ y: 0 }}
+      animate={{ y: 0 }}
+      transition={{ duration: 0.3, ease: 'circIn' }}>
+      <Card className="fixed top-16 right-8 px-3 py-3">
+        <div className="flex items-center text-muted-foreground">
+          <span className="text-sm mr-3 font-bold">Uploading Files</span>
+          <X
+            size={12}
+            onClick={() => setIsOpen(false)}
+            className="ml-auto flex cursor-pointer"
+          />
         </div>
-        <div className="mt-2 flex flex-col gap-2">
+        <div className="mt-3 flex flex-col gap-3">
           {uploadingFiles.map((file) => (
-            <div key={file.id}>
-              <p>{file.fileName}</p>
+            <div
+              key={file.id}
+              className="animate-pulse text-sm text-muted-foreground flex items-center w-48 ">
+              <Paperclip className="mr-2" size={14} />{' '}
+              <p className="text-ellipsis overflow-hidden">{file.fileName}</p>
             </div>
           ))}
         </div>
